325108e2bdc50a596cabf6309b7efffdf0158db3
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-03-29 17:41:29 +0200

    userStory1

---
+++ components/BudgetBar/BudgetBar.js
@@ -0,0 +1,15 @@
+import styles from "./BudgetBar.module.css";
+
+export default function Budgetbar() {
+  return (
+    <div className={styles.basicBar}>
+      <div className={styles.date}>
+        <div>Februar</div>
+        <div>2022</div>
+        <div>05</div>
+      </div>
+      <div className={styles.item}>Sony Playstation</div>
+      <div className={styles.amount}>500 Euro</div>
+    </div>
+  );
+}
---
+++ stories/components/BudgetBar.stories.jsx
@@ -0,0 +1,8 @@
+import BudgetBar from "../../components/BudgetBar/BudgetBar";
+
+export default {
+    title:"Components/BudgetBar",
+    component: BudgetBar,
+};
+
+export const budgetBar  = () => <BudgetBar />
