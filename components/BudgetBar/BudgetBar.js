97d5683e3cc1d99f9cd9ed8ee4187bc3acb64061
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-03-31 01:08:00 +0200

    wrapper div added

--- components/BudgetBar/BudgetBar.js
+++ components/BudgetBar/BudgetBar.js
@@ -2,14 +2,25 @@ import styles from "./BudgetBar.module.css";
 
 export default function Budgetbar() {
   return (
-    <button className={styles.basicBar}>
-      <div className={styles.date}>
-        <div>Februar</div>
-        <div>2022</div>
-        <div>05</div>
-      </div>
-      <div className={styles.item}>Sony Playstation</div>
-      <div className={styles.amount}>500 Euro</div>
-    </button>
+    <div className={styles.wrapperDiv}>
+      <button className={styles.basicBar}>
+        <div className={styles.date}>
+          <div>Februar</div>
+          <div>2022</div>
+          <div>05</div>
+        </div>
+        <div className={styles.item}>Sony Playstation</div>
+        <div className={styles.amount}>500 Euro</div>
+      </button>
+      <button className={styles.basicBar}>
+        <div className={styles.date}>
+          <div>März</div>
+          <div>2022</div>
+          <div>08</div>
+        </div>
+        <div className={styles.item}>Mercedes AMG</div>
+        <div className={styles.amount}>20.000 Euro</div>
+      </button>
+    </div>
   );
 }
