9278b7c402ca6e31b36a9fda729fd3ca1c45e0dc
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-03-31 00:07:19 +0200

    final responsive extras

--- components/BudgetBar/BudgetBar.js
+++ components/BudgetBar/BudgetBar.js
@@ -2,16 +2,14 @@ import styles from "./BudgetBar.module.css";
 
 export default function Budgetbar() {
   return (
-    <div className={styles.wrapperDiv}>
-      <button className={styles.basicBar}>
-        <div className={styles.date}>
-          <div>Februar</div>
-          <div>2022</div>
-          <div>05</div>
-        </div>
-        <div className={styles.item}>Sony Playstation</div>
-        <div className={styles.amount}>500 Euro</div>
-      </button>
-    </div>
+    <button className={styles.basicBar}>
+      <div className={styles.date}>
+        <div>Februar</div>
+        <div>2022</div>
+        <div>05</div>
+      </div>
+      <div className={styles.item}>Sony Playstation</div>
+      <div className={styles.amount}>500 Euro</div>
+    </button>
   );
 }
