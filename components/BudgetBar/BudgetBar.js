8230920d54c8a050be9235201db7ffe5ef873033
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-03-31 10:59:25 +0200

    yup

--- components/BudgetBar/BudgetBar.js
+++ components/BudgetBar/BudgetBar.js
@@ -12,15 +12,6 @@ export default function Budgetbar() {
         <div className={styles.item}>Sony Playstation</div>
         <div className={styles.amount}>500 Euro</div>
       </button>
-      <button className={styles.basicBar}>
-        <div className={styles.date}>
-          <div>März</div>
-          <div>2022</div>
-          <div>08</div>
-        </div>
-        <div className={styles.item}>Mercedes AMG</div>
-        <div className={styles.amount}>20.000 Euro</div>
-      </button>
     </div>
   );
 }
