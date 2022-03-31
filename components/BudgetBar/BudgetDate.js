152b1889ba95f7823f1ff4f325406fe46674b1dc
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-03-31 13:12:47 +0200

    formatted date

--- components/BudgetBar/BudgetDate.js
+++ components/BudgetBar/BudgetDate.js
@@ -1,1 +1,16 @@
-export default function BudgetDate() {}
+export default function BudgetDate(pros) {
+  const month = new Date("2022-02-05T00:00:00Z").toLocaleString("de-de", {
+    month: "long",
+  });
+  const year = new Date("2022-02-05T00:00:00Z").getFullYear();
+  const day = new Date("2022-02-05T00:00:00Z").toLocaleString("de-de", {
+    day: "2-digit",
+  });
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
