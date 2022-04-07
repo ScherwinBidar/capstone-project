e37ada5aa5d2ea0685d91ff389ea51e3dcf5cbd0
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-04-07 16:33:37 +0200

    changes

--- components/NeueAusgabe/NeueAusgabe.js
+++ components/NeueAusgabe/NeueAusgabe.js
@@ -3,8 +3,21 @@ import styles from "./NeueAusgabe.module.css";
 export default function NeueAusgabe() {
   return (
     <>
-      <div className={styles.banner}>
-        <div>Neue Ausgabe</div>
+      <div className={styles.wrapper}>
+        <div className={styles.banner}>
+          <h1>Neue Ausgabe</h1>
+        </div>
+        <div className={styles.background}>
+          <div className={styles.buttonWrapper1}>
+            <button>Item</button>
+            <button>Preis</button>
+            <button>Datum</button>
+          </div>
+          <div className={styles.buttonWrapper2}>
+            <button className={styles.abbrechen}>Abbrechen</button>
+            <button className={styles.bestätigen}>Bestätigen</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Budgetbar from "../components/BudgetBar/BudgetBar";
 import Banner from "../components/Banner/Banner";
 import styles from "../styles/Home.module.css";
+import NeueAusgabe from "../components/NeueAusgabe/NeueAusgabe";
 
 export default function Home() {
   return (
