08e4d2d42453d1163906df1775494c3abc77e6cf
Scherwin Bidar <85336025+ScherwinBidar@users.noreply.github.com>
2022-05-02 21:08:10 +0200

    auth

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -7,7 +7,9 @@ export default function Banner() {
         <h1 className={styles.logo}>Budgetinator</h1>
       </div>
       <div className={styles.lowerContent}>
-        <button className={styles.signOut}>Sign Out</button>
+        <button className={styles.signOut}>
+          <a href="api/auth/logout">Logout</a>
+        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
 import Banner from "../components/Banner/Banner";
 import WrapBars from "../components/BudgetBar/WrapBars";
 import NeueAusgabe from "../components/NeueAusgabe/NeueAusgabe";
+import LandingPage from "../components/LandingPage/LandingPage";
 
 const exampleArray = [
   {
     id: "e0",
     item: "Hier stehen bald ihre Ausgaben",
     price: "0",
-    date: new Date(2022, 2, 5),
+    date: new Date(2022, 1, 0),
   },
 ];
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  console.log(user);
+
   const [userData, setUserData] = useState(exampleArray);
 
   function importUserInputHandler(userInput) {
@@ -24,11 +30,19 @@ export default function Home() {
     });
   }
 
+  if (user) {
+    return (
+      <>
+        <Banner />
+        <NeueAusgabe onAddUserInput={importUserInputHandler} />
+        <WrapBars expenses={userData} />
+      </>
+    );
+  }
+
   return (
     <>
-      <Banner />
-      <NeueAusgabe onAddUserInput={importUserInputHandler} />
-      <WrapBars expenses={userData} />
+      <LandingPage />
     </>
   );
 }
